9fc6c08881ce13f0664a8098e604ed2b9f4e3051
解易 <xieyi@aksofy.cn>
2020-07-26 10:33:52 +0800

    update

---
+++ batch_backtest.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+  from abupy import AbuFactorBuyBreak; # 买入策略
+  from abupy import AbuFactorSellBreak, AbuFactorAtrNStop, AbuFactorPreAtrNStop, AbuFactorCloseAtrNStop; # 卖出策略
+  from abupy import AbuSlippageBuyMean; # 买入价格策略
+  from abupy import AbuKellyPosition; # 买入仓位策略
+  # n天向上突破买入策略(加入买入价格策略)
+  buy_factors = [{'xd': 60, 'class': AbuFactorBuyBreak, 'slippage': AbuSlippageBuyMean},
+                 {'xd': 42, 'class': AbuFactorBuyBreak, 'slippage': AbuSlippageBuyMean}];
+  # n天向下突破卖出策略
+  # 止盈止损卖出策略
+  # 剧烈下跌平仓策略
+  # 移动止盈策略
+  sell_factors = [{'xd': 120, 'class': AbuFactorSellBreak},
+                  {'stop_loss_n': 0.5, 'stop_win_n': 3.0, 'class': AbuFactorAtrNStop},
+                  {'pre_atr_n': 1.0, 'class': AbuFactorPreAtrNStop},
+                  {'close_atr_n': 1.5, 'class': AbuFactorCloseAtrNStop}];
+  from abupy import AbuPickRegressAngMinMax, AbuPickStockPriceMinMax; # 选股因子
+  # pose gradient restriction on stocks
+  # pose price restriction on stocks
+  stock_pickers = [{'threshold_ang_min': 0.0, 'threshold_ang_max': 60.0, 'reversed': False, 'class': AbuPickRegressAngMinMax},
+                   {'threshold_price_min': 50.0, 'threshold_price_max': 100.0, 'reserved': False, 'class': AbuPickStockPriceMinMax}];
+  # candidates
+  from abupy import abu;
+  read_cash = 1000000;
+  choice_symbols = ['usNOAH', 'usSFUN', 'usBIDU', 'usAAPL', 'usGOOG', 'usTSLA', 'usWUBA', 'usVIPS'];
+  abu_result_tuple, kl_pd_manager = abu.run_loop_back(read_cash, buy_factors, sell_factors, stock_pickers, choice_symbols, n_folds = 2);
+  
