6b2e077841bdb2606ee6c031c2d34aeffe89d589
解易 <xieyi@aksofy.cn>
2020-07-26 18:31:11 +0800

    update

--- grid_search.py
+++ grid_search.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
   import numpy as np;
   from abupy.CoreBu import ABuEnv;
   ABuEnv.g_market_source = ABuEnv.EMarketSourceType.E_MARKET_SOURCE_sn_us;
-  # 1) generate parameter combinations
+  # 1) generate parameter combinations for sell strategies
   from abupy import AbuFactorSellBreak, AbuFactorAtrNStop, AbuFactorPreAtrNStop, AbuFactorCloseAtrNStop; # 卖出策略
   sell_break_factor_grid = {
     'class': [AbuFactorSellBreak,],
-    'xd': np.arange(100, 150, 1), # xd_range = [100, 150)
+    'xd': np.arange(20, 150, 10), # xd_range = [20, 150)
   };
   sell_atr_nstop_factor_grid = {
     'class': [AbuFactorAtrNStop,],
@@ -30,4 +30,24 @@ if __name__ == "__main__":
     [sell_break_factor_grid, sell_atr_nstop_factor_grid, sell_atr_pre_factor_grid, sell_atr_close_factor_grid]
   );
   print('combination number is {}'.format(len(sell_factors_product)));
-  # 2)
+  # 2) generate parameters combinations for buy strategies
+  from abupy import AbuFactorBuyBreak; # 买入策略
+  buy_bk_factor_grid = {
+    'class': [AbuFactorBuyBreak,],
+    'xd': [42, 60],
+  }
+  buy_factors_product = ABuGridHelper.gen_factor_grid(
+    ABuGridHelper.K_GEN_FACTOR_PARAMS_BUY,
+    [buy_bk_factor_grid, buy_bk_factor_grid]
+  );
+  print('combination number is {}'.format(len(buy_factors_product)));
+  # 3) grid search
+  from abupy import GridSearch;
+  read_cash = 1000000;
+  choice_symbols = ['usNOAH', 'usSFUN', 'usBIDU', 'usAAPL', 'usGOOG', 'usTSLA', 'usWUBA', 'usVIPS'];
+  grid_search = GridSearch(read_cash, choice_symbols, buy_factors_product = buy_factors_product, sell_factors_product = sell_factors_product);
+  scores, score_tuple_array = grid_search.fit(n_jobs = -1);
+  best_score_tuple_grid = grid_search.best_score_tuple_grid;
+  from abupy import AbuMetricsBase;
+  AbuMetricsBase.show_general(best_score_tuple_grid.orders_pd, best_score_tuple_grid.action_pd, best_score_tuple_grid.capital, best_score_tuple_grid.benchmark);
+  
