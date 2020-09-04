5d87706a9b7c7010da3bdb7d2499ac7a8ac46288
root <root@LAPTOP-A2EG90UI.localdomain>
2020-09-04 14:04:29 +0800

    update

--- vnpy/save_model.py
+++ vnpy/save_model.py
@@ -6,6 +6,7 @@ from tf_agents.networks.actor_distribution_rnn_network import ActorDistributionR
 from tf_agents.networks.value_rnn_network import ValueRnnNetwork;
 from tf_agents.agents.ppo import ppo_agent;
 from tf_agents.trajectories.time_step import TimeStep, StepType, time_step_spec;
+from tf_agents.utils.common import Checkpointer;
 from tf_agents.policies import policy_saver;
 
 def save_model():
