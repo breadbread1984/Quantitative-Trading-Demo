e5093a4fea38efbea58058c5495e91e1725ae797
root <root@LAPTOP-A2EG90UI.localdomain>
2020-09-18 16:54:19 +0800

    update

--- vnpy/max_sharpe/train.py
+++ vnpy/max_sharpe/train.py
@@ -16,20 +16,17 @@ def train():
   bars = database_manager.load_bar_data('IF88', Exchange.CFFEX, Interval.MINUTE, datetime(2009,1,1), datetime(2020,8,8));
   mu = 1000000 / 300; # how many possible positions
   delta = 0.5 / 10000; # rate
-  F_tm1 = 0;
+  F_tm1 = tf.zeros((1,1), dtype = tf.float32);
   history_length = len(bars);
   r = [bars[i].close_price - bars[i-1].close_price for i in range(1, len(bars))]; # r.shape = (history_length - 1,)
   R = list();
   for i in range(100):
     with tf.GradientTape() as tape:
       for i in range(len(r) - 11 + 1):
-        xt = np.ones((13,), dtype = np.float32);
-        xt[1:12] = r[i:i+11];
-        xt[12] = F_tm1;
-        xt = tf.expand_dims(xt, axis = 0); # xt.shape = (1, 13)
-        F_t = predictor(xt);
-        R.append(mu * (F_tm1 * r[i] - delta * tf.math.abs(F_t[0,0] - F_tm1)));
-        F_tm1 = F_t[0,0];
+        xt = tf.concat([tf.ones((1,1), dtype = tf.float32), tf.expand_dims(r[i:i+11], axis = 0), F_tm1], axis = -1); # xt.shape = (1, 13)
+        F_t = predictor(xt); # F_t.shape = (1, 1)
+        R.append(mu * (F_tm1 * r[i] - delta * tf.math.abs(F_t - F_tm1)));
+        F_tm1 = F_t;
       sharpe_rate = tf.math.reduce_mean(R) / tf.math.sqrt(tf.math.reduce_mean(tf.math.square(R)) - tf.math.square(tf.math.reduce_mean(R)));
       loss = -sharp_rate;
     grads = tape.gradient(loss, predictor.trainable_variables);
